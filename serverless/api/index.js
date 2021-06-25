0c93db21155a59dd759c71bf17d5aec77b41ef2a
Carlos Alvarez <car.alv2804@gmail.com>
2021-06-25 12:30:30 -0500

    creating models Meals, Orders

--- serverless/api/index.js
+++ serverless/api/index.js
@@ -1,3 +1,18 @@
-module.exports = (req, res) => {
-    res.send('Hello World!')
-}
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+
+// use is for adding pl(ugis on the server
+app.use(bodyParser.json())
+app.use(cors())
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+
+app.get('*', (req, res) => {
+    res.send('Chanchito feliz!')
+})
+module.exports = app
